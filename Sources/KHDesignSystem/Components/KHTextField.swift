19e2d0e7848a3e682fec89624f6a92492034dd1f
Kano <coke8707@gmail.com>
2024-07-28 14:56:17 +0900

    feat : TextField 추가

--- Sources/KHDesignSystem/Components/KHTextField.swift
+++ Sources/KHDesignSystem/Components/KHTextField.swift
@@ -1,18 +1,40 @@
 //
-//  SwiftUIView.swift
-//  
+//  KHTextField.swift
+//
 //
 //  Created by LEE on 7/28/24.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct KHTextField: View {
+    @Binding var text:String
+    
+    var placeHolder:String?
+    var symboldIcon:Image?
+    var keyboardType:UIKeyboardType = .default
+    var font:Font = KHFont.subTitle02
+    @FocusState private var isFocused: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            symboldIcon.foregroundColor(isFocused ? KHColor.Primary.P00 : .gray).frame(width:44, height: 44)
+            TextField(placeHolder ?? "", text: $text).keyboardType(keyboardType).font(font)
+        }.background(
+            
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(isFocused ? KHColor.Primary.P00 : KHColor.Gray.GR30, lineWidth: isFocused ? 2.0 : 1.5)
+        )
+        .focused($isFocused)
+        
     }
 }
 
 #Preview {
-    SwiftUIView()
+    @State var text:String = ""
+    
+    let tf = KHTextField(text: $text, placeHolder: "텍스트를 입력하세요", symboldIcon: Image(systemName: "magnifyingglass"))
+    print(text)
+    return tf
+    
 }
